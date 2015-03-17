ee006db918dab14cdee38da9bf1e6a39972e198c
Derek Ostrander <djostran@gmail.com>
2015-03-17 14:20:16 -0400

    Fixed tests trying to use RZAutolayoutHelpers

---
+++ Example/Pods/Target Support Files/Pods-RZIntrinsicContentSizeTextView-RZUtils/Pods-RZIntrinsicContentSizeTextView-RZUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RZIntrinsicContentSizeTextView_RZUtils : NSObject
+@end
+@implementation PodsDummy_Pods_RZIntrinsicContentSizeTextView_RZUtils
+@end
